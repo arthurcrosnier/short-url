6021ec84f31450f917c3396a5745465d31b4c075
Arhur Crosnier <arthur crosnier>
2024-12-11 19:07:26 +0100

    check in server if url is valid, activate the nest global validation pipe

--- backend/scripts/test-create-url.sh
+++ backend/scripts/test-create-url.sh
@@ -1,6 +1,11 @@
-# scripts/test-create-url.sh
-
+# URL valide - devrait marcher
 curl -X POST http://localhost:3000/shortener \
   -H "Content-Type: application/json" \
-  -d '{"originalUrl": "https://www.example.com/very/long/url"}' \
+  -d '{"originalUrl": "https://www.example.com"}' \
+  | json_pp
+
+# URL invalide - devrait échouer
+curl -X POST http://localhost:3000/shortener \
+  -H "Content-Type: application/json" \
+  -d '{"originalUrl": "test"}' \
   | json_pp
